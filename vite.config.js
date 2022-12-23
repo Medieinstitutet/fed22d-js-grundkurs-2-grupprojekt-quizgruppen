04d75603686ab1b10e2c4c7c7ee18e86c6968cad
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2022-12-23 14:46:13 +0100

    Added repo name

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ då ska "base" här nedan vara "/fed22d-js-grundkurs-jenni-wumpus/"
  */
 
 export default defineConfig({
-  base: '/js-template/', // TODO - ändra till ditt repo-namn
+  base: '/fed22d-js-grundkurs-2-grupprojekt-quizgruppen/', // TODO - ändra till ditt repo-namn
   define: {
     'process.env': {},
   },
