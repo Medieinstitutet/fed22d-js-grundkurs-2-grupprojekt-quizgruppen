bfda52255e1ce9d783faed2b20e5f4247d2536a9
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2022-12-23 14:50:16 +0100

    Test gh-pages

--- src/main.js
+++ src/main.js
@@ -250,7 +250,6 @@ function chosenQuiz() {
       questionTimer = 15;
       scoreGain = 3;
     } 
-
   } else if (category == 'computer') {
     if (difficulty == 'easy') {
       question = computerQuestionsEasy[questionCounter].question;
